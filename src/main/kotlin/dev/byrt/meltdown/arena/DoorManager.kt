091345efe12a103e9b4b84d78026822b4754d335
ByrtZ <taylor.byrne417@gmail.com>
2024-02-21 13:36:41 +0000

    Set up for coin crates and doors.

---
+++ src/main/kotlin/dev/byrt/meltdown/arena/DoorManager.kt
@@ -0,0 +1,4 @@
+package dev.byrt.meltdown.arena
+
+class DoorManager {
+}
---
+++ src/main/kotlin/dev/byrt/meltdown/data/Door.kt
@@ -0,0 +1,17 @@
+package dev.byrt.meltdown.data
+
+import org.bukkit.Location
+
+data class Door(val id : Int, val corner1 : Location, val corner2 : Location, val doorType : DoorType, var doorStatus: DoorStatus)
+
+enum class DoorType {
+    CENTRE,
+    DIVIDER,
+    GENERIC
+}
+
+enum class DoorStatus {
+    OPEN,
+    CLOSED,
+    CLOSING
+}
